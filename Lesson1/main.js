456a0fd104a5a895503794f0866ca07984819628
Mibchuani <mibchuani@gmail.com>
2022-11-01 15:57:22 +0200

    add js

---
+++ Lesson1/main.js
@@ -0,0 +1,58 @@
+//'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+
+let abc ='hello';
+console.log(abc);
+
+abc ='owu';
+console.log(abc);
+
+let ced ='com'
+console.log(ced);
+
+let edf ='ua'
+console.log(edf)
+
+let num1 =1
+console.log(num1);
+
+num1=10
+console.log(num1)
+
+let num3=-999
+console.log(num3);
+
+let num4=123
+console.log(num4);
+
+const PI=3.14
+console.log(PI);
+
+const E=2.7
+console.log(E);
+
+num3=16
+console.log(num3);
+
+let asd = 5<6
+console.log(asd);
+
+asd = 5>6
+console.log(asd);
+
+//Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+// З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+let firstName = 'Олена'
+let middleName = 'Зелімханівна'
+let lastName = 'Мібчуані'
+console.log(`${firstName} ${middleName} ${lastName}`)
+
+//- За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//   let a = 100; let b = '100'; let c = true;
+
+let a = 100;
+let b = '100'
+let c = 100 =='100';
+console.log(c);
+
+
