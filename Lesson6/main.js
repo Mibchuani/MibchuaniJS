1f39abd043c740cea60694e6c2ff7d9c2573a02b
Mibchuani <mibchuani@gmail.com>
2022-11-25 16:53:01 +0200

    add js hw6

--- Lesson6/main.js
+++ Lesson6/main.js
@@ -46,8 +46,8 @@ console.log(stringToarray);
 // стрінгові.
 
 let arr = [10,8,-7,55,987,-1011,0,1050,0]
-//let strArr = arr.map((numbers) => ({''}))
-//console.log(strArr)
+let strArr = arr.map(value => value.toString())
+console.log(strArr)
 
 // - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого,
 // або навпаки в залежності від значення аргументу direction.
@@ -55,20 +55,29 @@ let arr = [10,8,-7,55,987,-1011,0,1050,0]
 // sortNums(nums,'ascending') // [3,11,21]
 // sortNums(nums,'descending') // [21,11,3]
 let nums = [11,21,3]
-let sortNums = nums.sort(function (a,b) {
-  if (a<b) {
-    return -1
-  }
-  if (a>b) {
-    return 1
-  }
-  if (a === b){
-    return 0
-  }
-})
-console.log(sortNums);
-
-console.log(nums.reverse());
+//let sortNums = nums.sort(function (a,b) {
+  //if (a<b) {
+   // return -1
+  //}
+ // if (a>b) {
+ //   return 1
+  //}
+  //if (a === b){
+    //return 0
+ // }
+//})
+//console.log(sortNums);
+//console.log(nums.reverse());
+const sortNums = (direction, arr11) => {
+if (direction === 'ascending') {
+  arr11.sort((a, b) => a-b)
+} else if (direction ==='descending') {
+  arr11.sort((a, b) => b-a)
+}
+return arr11
+}
+console.log(sortNums('ascending', nums))
+console.log(sortNums('descending', nums))
 
 // - є масив
 // let coursesAndDurationArray = [
@@ -98,11 +107,10 @@ let filterUs = coursesAndDurationArray.filter(value=> value.monthDuration > 5)
 console.log(filterUs);
 
 //  описати колоду карт
-//  - знайти піковий туз
-//  - всі шістки
-//  - всі червоні карти
-//  - всі буби
-//  - всі трефи від 9 та більше
+
+
+
+
 
 // {
 //     cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
@@ -110,38 +118,97 @@ console.log(filterUs);
 //     color:'', // 'red','black'
 // }
 
-let cardSuits = ['spade', 'diamond','heart', 'clubs']
-let values = ['6', '7', '8', '9', '10', 'ace','jack','queen','king','joker']
-let colors = ['red','black']
-let cards1 = []
-for (let i1 = 0; i1 < cardSuits.length; i1++)
-for (let i2 = 0; i2 < values.length-1; i2++)
+//let cardSuits = ['spade', 'diamond','heart', 'clubs']
+//let values = ['6', '7', '8', '9', '10', 'ace','jack','queen','king','joker']
+//let colors = ['red','black']
+//let cards1 = []
+//for (let i1 = 0; i1 < cardSuits.length; i1++)
+//for (let i2 = 0; i2 < values.length-1; i2++)
 
-  {
-    let card1 = {cardSuit: cardSuits[i1], value: values[i2]}
-    cards1.push(card1)
-  }
-console.log(cards1);
+  //{
+    //let card1 = {cardSuit: cardSuits[i1], value: values[i2]}
+   // cards1.push(card1)
+  //}
+//console.log(cards1);
 
-let cards2 = []
-for (let i2 = 9; i2 < values.length; i2++)
-for (let i3 = 0; i3 < colors.length; i3++){
-  let card2 = {value: values.length[i2], color: colors[i3]}
-  cards2.push(card2)
-}
-console.log(cards2);
+//let cards2 = []
+//for (let i2 = 9; i2 < values.length; i2++)
+//for (let i3 = 0; i3 < colors.length; i3++){
+  //let card2 = {value: values.length[i2], color: colors[i3]}
+  //cards2.push(card2)
+//}
+//console.log(cards2);
 
-let cards =[]
-cards.concat(cards1, cards2)
-console.log(cards);
-
-let cards1filter = cards1.filter((card6) => cards1.value === 6)
-console.log(cards1filter)
-
-//let cardSort = cards.sort('spade', 'ace')
-//console.log(cardSort);
+//let cards =[]
+//cards.concat(cards1, cards2)
+//console.log(cards);
 
 
+
+let deck = [
+  {cardSuit:'Spade', value: 6, color:'Black'},
+  {cardSuit:'Clubs', value: 6, color:'Black'},
+  {cardSuit:'Hearts', value: 6, color:'Red'},
+  {cardSuit:'Diamonds', value: 6, color:'Red'},
+  {cardSuit:'Spade', value: 7, color:'Black'},
+  {cardSuit:'Clubs', value: 7, color:'Black'},
+  {cardSuit:'Hearts', value: 7, color:'Red'},
+  {cardSuit:'Diamonds', value: 7, color:'Red'},
+  {cardSuit:'Spade', value: 8, color:'Black'},
+  {cardSuit:'Clubs', value: 8, color:'Black'},
+  {cardSuit:'Hearts', value: 8, color:'Red'},
+  {cardSuit:'Diamonds', value: 8, color:'Red'},
+  {cardSuit:'Spade', value: 9, color:'Black'},
+  {cardSuit:'Clubs', value: 9, color:'Black'},
+  {cardSuit:'Hearts', value: 9, color:'Red'},
+  {cardSuit:'Diamonds', value: 9, color:'Red'},
+  {cardSuit:'Spade', value: 10, color:'Black'},
+  {cardSuit:'Clubs', value: 10, color:'Black'},
+  {cardSuit:'Hearts', value: 10, color:'Red'},
+  {cardSuit:'Diamonds', value: 10, color:'Red'},
+  {cardSuit:'Spade', value: 'Jack', color:'Black'},
+  {cardSuit:'Clubs', value: 'Jack', color:'Black'},
+  {cardSuit:'Hearts', value: 'Jack', color:'Red'},
+  {cardSuit:'Diamonds', value: 'Jack', color:'Red'},
+  {cardSuit:'Spade', value: 'Queen', color:'Black'},
+  {cardSuit:'Clubs', value: 'Queen', color:'Black'},
+  {cardSuit:'Hearts', value: 'Queen', color:'Red'},
+  {cardSuit:'Diamonds', value: 'Queen', color:'Red'},
+  {cardSuit:'Spade', value: 'King', color:'Black'},
+  {cardSuit:'Clubs', value: 'King', color:'Black'},
+  {cardSuit:'Hearts', value: 'King', color:'Red'},
+  {cardSuit:'Diamonds', value: 'King', color:'Red'},
+  {cardSuit:'Spade', value: 'Ace', color:'Black'},
+  {cardSuit:'Clubs', value: 'Ace', color:'Black'},
+  {cardSuit:'Hearts', value: 'Ace', color:'Red'},
+  {cardSuit:'Diamonds', value: 'Ace', color:'Red'},
+  {cardSuit:null, value: 'Jocker', color:'Red'},
+  {cardSuit:null, value: 'Jocker', color:'Black'}
+];
+
+//  - знайти піковий туз
+
+let filter1 = deck.filter(value => value.cardSuit === 'Spade' && value.value === 'Ace');
+console.log(filter1);
+
+//  - всі шістки
+
+let filter2 = deck.filter(value => value.value === 6)
+console.log(filter2)
+
+//  - всі червоні карти
+
+let filter3 = deck.filter(value => value.color === 'Red')
+console.log(filter3)
+
+//  - всі буби
+let filter4 = deck.filter(value => value.cardSuit === 'Diamonds')
+console.log(filter4)
+//  - всі трефи від 9 та більше
+
+let filter5 = deck.filter(value => (value.cardSuit === 'Clubs' && value.value > 8) ||
+    (value.cardSuit === 'Clubs' && typeof value.value === 'string'))
+console.log(filter5)
 
 // Додатково по reduce
 // Взяти описану колоду карт, та за допомоги редюсу попакувати всі карти по "мастях" в об'єкт
@@ -152,3 +219,23 @@ console.log(cards1filter)
 //     clubs:[]
 // }
 
+let reduce = deck.reduce((accum, value) => {
+  if (value.cardSuit === 'Spade') {
+    accum.spades.push(value);
+  } else if (value.cardSuit === 'Clubs') {
+    accum.clubs.push(value);
+  } else if (value.cardSuit === 'Diamonds') {
+    accum.diamonds.push(value);
+  } else if (value.cardSuit === 'Hearts') {
+    accum.hearts.push(value);
+  }
+  return accum
+},
+{
+  spades: [],
+diamonds: [],
+hearts: [],
+clubs: []
+
+})
+console.log(reduce);
