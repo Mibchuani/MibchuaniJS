ae668673e6537fcccc1135640844db65ad2b1e0e
Mibchuani <mibchuani@gmail.com>
2022-12-23 14:41:53 +0200

    add mini-project js

---
+++ Mini-project/main1.js
@@ -0,0 +1,33 @@
+let url = new URL(location.href);
+let id = url.searchParams.get('data')
+fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+.then(value => value.json())
+.then(value => {
+    for (const item in value) {
+        let userDiv = document.createElement('div');
+        if (typeof value[item] !== 'object') {
+            userDiv.innerText = `${item} ${value[item]}`;
+        } else {
+            userDiv.innerText = `${item}`;
+            for (const key in value[item]) {
+                let userInnerDiv = document.createElement('div')
+if (typeof value[item][key] !== 'object') {
+    userInnerDiv.innerText = `${key} -- ${value[item][key]}`;
+} else {
+    userInnerDiv.innerText = `${key} :`;
+    for (const element of value[item][key]) {
+        let htmlDivElement = document.createElement('div');
+        if (typeof value[item][key][element] !== 'object'){
+            htmlDivElement.innerText = `${element} -- ${value[item][key][element]}`
+        }
+userInnerDiv.append(htmlDivElement)
+        
+    }
+}
+userDiv.append(userInnerDiv);
+            }
+        }
+
+        document.body.append(userDiv);
+    }
+});
